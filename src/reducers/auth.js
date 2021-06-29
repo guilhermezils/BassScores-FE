6f029571d9b3c9f03713c566ed1e1127d181399c
guilhermezils <guilhermezils@icloud.com>
2021-06-29 14:13:35 -0500

    comment out some files

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,42 +1,44 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, CREATE_USER_SUCCESS, REGISTER_FAIL } from '../actions/types';
 
-const initialState = {
-    token: localStorage.getItem('token'),
-    isAuthenticated: localStorage.getItem('token') ? true : false, // or just !!localStorage.getItem('token')
-    isLoading: false,
-    isRegistered: false
- }
 
-export default function(state = initialState, action) {
-  switch(action.type) {
-    case CREATE_USER_SUCCESS:
-      return {
-        ...state,
-        ...action.payload,
-        token: null,
-        isAuthenticated: false,
-        isLoading: false,
-        isRegistered: true
-      }
-    case LOGIN_SUCCESS:
-      localStorage.setItem('token', action.payload.token);
-      return {
-        ...state,
-        ...action.payload,
-        isAuthenticated: true,
-        isLoading: false,
-      }
-    case LOGIN_FAIL:
-    case LOGOUT_SUCCESS:
-    case REGISTER_FAIL:
-      localStorage.removeItem('token');
-      return {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        isLoading: false
-      }
-    default:
-      return state;
-  }
-}
+// import { LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, CREATE_USER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+
+// const initialState = {
+//     token: localStorage.getItem('token'),
+//     isAuthenticated: localStorage.getItem('token') ? true : false, // or just !!localStorage.getItem('token')
+//     isLoading: false,
+//     isRegistered: false
+//  }
+
+// export default function(state = initialState, action) {
+//   switch(action.type) {
+//     case CREATE_USER_SUCCESS:
+//       return {
+//         ...state,
+//         ...action.payload,
+//         token: null,
+//         isAuthenticated: false,
+//         isLoading: false,
+//         isRegistered: true
+//       }
+//     case LOGIN_SUCCESS:
+//       localStorage.setItem('token', action.payload.token);
+//       return {
+//         ...state,
+//         ...action.payload,
+//         isAuthenticated: true,
+//         isLoading: false,
+//       }
+//     case LOGIN_FAIL:
+//     case LOGOUT_SUCCESS:
+//     case REGISTER_FAIL:
+//       localStorage.removeItem('token');
+//       return {
+//         ...state,
+//         token: null,
+//         isAuthenticated: false,
+//         isLoading: false
+//       }
+//     default:
+//       return state;
+//   }
+// }
